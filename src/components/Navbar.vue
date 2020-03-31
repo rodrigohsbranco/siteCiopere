ab5fc816ad4a7ab5775690eae3111d099de2f95c
Luiz <lh.alvesf@gmail.com>
2020-03-31 12:48:45 -0300

    nova estrutura de componentização

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="mainNavbar">
+        <h2>Esta é a minha navbar {{ exemplo }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            exemplo: 'um novo dado'
+        }
+    }
+}
+</script>
+
+<style>
+    #mainNavbar {
+        background: red;
+    }
+</style>
